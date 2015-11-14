ec5e6bdaac0f8272578df451819278eadef62ed5
xuxihui <xuxihui92@gmail.com>
2015-11-14 14:43:34 -0600

    Remove Invalid Parenthese

---
+++ Remove Invalid Parentheses/src/com/company/Main.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String s = reader.nextLine();
+        List<String> res;
+        res = removeInvalidParentheses(s);
+        System.out.println(res);
+    }
+
+    public static List<String> removeInvalidParentheses(String s) {
+        List<String> res = new ArrayList<>();
+        if (s == null || s.length() == 0) {
+            res.add("");
+            return res;
+        }
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+
+        // initialize
+        queue.add(s);
+        visited.add(s);
+        boolean found = false;
+
+        while (!queue.isEmpty()) {
+            s = queue.poll();
+
+            if (isValid(s)) {
+                // found an answer, add to the result
+                res.add(s);
+                found = true;
+            }
+
+            if (found) continue;
+
+            // generate all possible states
+            for (int i = 0; i < s.length(); i++) {
+                // we only try to remove left or right paren
+                if (s.charAt(i) != '(' && s.charAt(i) != ')') continue;
+
+                String t = s.substring(0, i) + s.substring(i + 1);
+
+                if (!visited.contains(t)) {
+                    // for each state, if it's not visited, add it to the queue
+                    queue.add(t);
+                    visited.add(t);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            }else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
